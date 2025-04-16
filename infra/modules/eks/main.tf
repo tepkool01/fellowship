c0b979d9358ff8a276b3c342b6a5085820147da1
teppy <mvy1221@gmail.com>
2025-04-16 15:55:09 -0700

    fix

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,17 +1,16 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 19.0"
   cluster_name    = "${var.name_prefix}-cluster"
   cluster_version = "1.28"
-  subnets         = aws_subnet.public[*].id
+  subnet_ids      = aws_subnet.public[*].id
   vpc_id          = module.vpc.id
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-      instance_types = ["t3.small"]
+      desired_size    = 2
+      max_size        = 3
+      min_size        = 1
+      instance_types  = ["t3.small"]
     }
   }
 }
