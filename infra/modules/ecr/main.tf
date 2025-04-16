9d797c720c208f4edb4c5dd3f555cafc10710c5e
teppy <mvy1221@gmail.com>
2025-04-16 16:09:42 -0700

    ignoring created repo

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,12 @@
 resource "aws_ecr_repository" "this" {
   name = var.repo_name
+
   image_scanning_configuration {
     scan_on_push = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name]
   }
 }
